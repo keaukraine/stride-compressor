1757cd77589a261dd3230f0716f2f2fa91cf55a1
keaukraine <keaukraine@gmail.com>
2020-06-11 23:14:51 +0300

    Implemented paddings and packed INT_2_10_10_10_REV.

--- src/compressor.js
+++ src/compressor.js
@@ -3,8 +3,13 @@ const yargs = require("yargs");
 const fs = require("fs");
 const path = require("path");
 
-const intValue = -3;
+/**
+ * Usage:
+ * node src/compressor.js buddha-strides.bin --stride=FFF-hh-hh-hhh
+ */
 
+
+/** Returns a string representation of 32-bit number. */
 function dec2bin(dec) {
     let str = (dec >>> 0).toString(2);
     for (; str.length < 32;) {
@@ -13,24 +18,15 @@ function dec2bin(dec) {
     return str;
 }
 
-const x = intValue & 0x000003FF;
-const y = intValue << 10 & 0x000FFC00;
-const z = intValue << 20 & 0x3FF00000;
-
-console.log(dec2bin(intValue));
-console.log(dec2bin(x));
-console.log(dec2bin(y));
-console.log(dec2bin(z));
-console.log(dec2bin(x | y | z));
-
-console.log("wwzzzzzzzzzzyyyyyyyyyyxxxxxxxxxx")
-
-process.exit(0);
-
-
 /**
- * node src/compressor.js buddha-strides.bin --stride=FFF-hh-hh-hhh
+ * Packs 3 floats to INT_2_10_10_10_REV structure.
  */
+function pack3Floats(x, y, z) {
+    const px = (Math.floor(x * 511)) & 0x000003FF;
+    const py = (Math.floor(y * 511)) << 10 & 0x000FFC00;
+    const pz = (Math.floor(z * 511)) << 20 & 0x3FF00000;
+    return px | py | pz;
+}
 
 const argv = yargs.argv;
 const strideFormat = argv.stride;
@@ -41,6 +37,11 @@ const types = [];
 let i = 0;
 for (const type of strideFormat) {
     switch (type) {
+        case "0": // one byte padding
+            outStrideSize += 1;
+            types[i++] = "padding";
+            break;
+        case "F":
         case "p":
             strideSize += 3;
             outStrideSize += 4;
@@ -75,7 +76,7 @@ for (const type of strideFormat) {
 
 const padding = (Math.ceil(outStrideSize / 4) * 4) - outStrideSize;
 
-console.log(`Original stride size ${strideSize * 4} bytes; new stride size ${outStrideSize + padding} bytes (including padding last ${padding} bytes).`);
+console.log(`Original stride size ${strideSize * 4} bytes; new stride size ${outStrideSize + padding} bytes (last ${padding} bytes are empty).`);
 
 fs.readFile(argv._[0], (err, data) => {
     const input = new Float32Array(data.buffer, 0, data.byteLength / 4);
@@ -84,26 +85,48 @@ fs.readFile(argv._[0], (err, data) => {
     for (let i = 0; i < input.length / strideSize; i++) {
         const outStrideStart = i * (outStrideSize + padding);
         let outStrideOffset = 0;
-        for (let j = 0; j < strideSize; j++) {
+        let j = 0;
+        for (const type of types) {
             const floatValue = input[i * strideSize + j];
-            if (types[j] === "float") {
+            if (type === "padding") {
+                outStrideOffset += 1;
+            } else if (type === "float") {
                 const view = new Float32Array(result, outStrideStart + outStrideOffset, 1);
                 view[0] = floatValue;
                 outStrideOffset += 4;
-            } else if (types[j] === "half") {
+                j++;
+            } else if (type === "half") {
                 const view = new Float16Array(result, outStrideStart + outStrideOffset, 1);
                 view[0] = floatValue;
                 outStrideOffset += 2;
-            } else if (types[j] === "sbyte") {
+                j++;
+            } else if (type === "sbyte") {
                 const view = new Int8Array(result, outStrideStart + outStrideOffset, 1);
                 // Expect value to be in -1...1 range
                 view[0] = Math.floor(floatValue * 127);
                 outStrideOffset += 1;
-            } else if (types[j] === "ubyte") {
+                j++;
+            } else if (type === "ubyte") {
                 const view = new Uint8Array(result, outStrideStart + outStrideOffset, 1);
                 // Expect value to be in 0...1 range
                 view[0] = Math.floor(floatValue * 255);
                 outStrideOffset += 1;
+                j++;
+            } else if (type === "packed3") {
+                const temp = new Uint32Array(1);
+                const tempView = new Uint8Array(temp.buffer);
+                const view = new Uint8Array(result, outStrideStart + outStrideOffset, 4);
+                temp[0] = pack3Floats(
+                    input[i * strideSize + j + 0],
+                    input[i * strideSize + j + 1],
+                    input[i * strideSize + j + 2]
+                ) >>> 0;
+                view[0] = tempView[0];
+                view[1] = tempView[1];
+                view[2] = tempView[2];
+                view[3] = tempView[3];
+                outStrideOffset += 4;
+                j += 3;
             }
         }
     }
