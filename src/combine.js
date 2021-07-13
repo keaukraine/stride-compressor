5780c5acab87cf44e60f13576eb6537289b1a95a
Oleksandr Popov <keaukraine@gmail.com>
2021-07-13 22:14:31 +0300

    Added fp16 / fp32 switch.

--- src/combine.js
+++ src/combine.js
@@ -6,30 +6,31 @@ const path = require("path");
 
 /**
  * Usage:
- * node src/combine.js name --frames=10 width=1024
+ * node src/combine.js name --frames=10 --width=1024 --size=[6|12]
  */
 
 const argv = yargs.argv;
 
 const name = argv._[0];
 const frames = argv.frames;
+const texelSize = argv.size; // 6 or 12
 const width = argv.width;
 
 const stat = fs.statSync(`${name}1-strides.bin`);
-const vertices = stat.size / 6;
+const vertices = stat.size / texelSize;
 const chunks = Math.ceil(vertices / width);
 const textureHeight = chunks * (frames + 1);
 
 console.log(`Vertices: ${vertices}; split into ${chunks} chunks.`);
 console.log(`Texture dimensions: ${width}x${textureHeight}.`);
 
-const result = new ArrayBuffer(width * 6 * (frames + 1) * chunks);
+const result = new ArrayBuffer(width * texelSize * (frames + 1) * chunks);
 
 const resultArray = [];
 
 for (let chunk = 0; chunk < chunks; chunk++) {
-    const bytesToRead = width * 6;
-    const startReadOffset = chunk * width * 6;
+    const bytesToRead = width * texelSize;
+    const startReadOffset = chunk * width * texelSize;
     // console.log(startReadOffset, bytesToRead);
 
     // read each frame
@@ -45,11 +46,11 @@ for (let chunk = 0; chunk < chunks; chunk++) {
     }
 
     // 1px padding between chunks
-    for (let i = 0; i < width * 6; i++) {
+    for (let i = 0; i < width * texelSize; i++) {
         resultArray.push(0);
     }
 }
 
 const parsedPath = path.parse(name);
-const outputFile = path.join(parsedPath.dir, parsedPath.name + ".rgb.fp16");
+const outputFile = path.join(parsedPath.dir, parsedPath.name + ".rgb.fp" + (texelSize === 6 ? "16" : "32"));
 fs.writeFileSync(outputFile, Buffer.from(resultArray));
